cc898692382cd3c8181c3a7cf1dc38302072fc0e
ruslan <wb.russel@outlook.com>
2014-08-01 19:46:59 +0600

    example

---
+++ index.js
@@ -0,0 +1,13 @@
+var app = module.exports = require('derby').createApp('d-material', __filename);
+
+app.serverUse(module, 'derby-stylus');
+app.use(require('d-material'));
+app.loadViews(__dirname);
+
+// Маршрут рендерится на клиене и на сервере
+app.get('/', function(page, model) {
+    // Подписка обеспечивает синхронизацию данных
+    model.subscribe('hello.message', function() {
+        page.render('home');
+    });
+});
