9f7c5997cb70bddaa4f8373a4b0009612958daf6
ruslan <wb.russel@outlook.com>
2014-08-23 20:18:18 +0600

    - fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ app.serverUse(module, 'derby-stylus');
 app.use(require('d-material'));
 app.loadViews(__dirname);
 
-// Маршрут рендерится на клиенте и на сервере
 app.get('/', function(page, model) {
     page.render('home');
 });
